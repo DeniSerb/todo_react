e1213756c5a1618a7390869495c40ca3acd79053
Denis <dondo788@gmail.com>
2018-02-20 11:39:40 +0200

    Add the basis

--- src/index.js
+++ src/index.js
@@ -2,26 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
 import App from './App';
 import './index.css';
+import reducer from './reducers';
 
-const initialState = [
-	'One',
-	'Two'
-];
 
-function playlist(state = initialState, action) {
-	if (action.type === 'ADD_TRACK') {
-		return [
-		...state,
-		action.payload
-		];
-	}
-	return state;
-}
 
-const store = createStore(playlist);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>
